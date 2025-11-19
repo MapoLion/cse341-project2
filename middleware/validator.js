228423cbec0c208d00b9ccdd0514e1ca79884c3e
John Helaman Chan <johnhelaman10@gmail.com>
2025-11-19 19:42:04 +0800

    Fixed a misattribute

--- middleware/validator.js
+++ middleware/validator.js
@@ -3,7 +3,7 @@ const validator = require('../helpers/validator');
 const validateBook = (req, res, next) => {
   const rules = {
     title: "required|string",
-    author: "required|array",
+    author: "required|sting",
     firstPublished: "string"
   };
 
